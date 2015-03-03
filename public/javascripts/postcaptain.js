366704a4cf59a5984c20bb1887cce2e6a48f5a0a
Russell DuBrock <russell.dubrock@gmail.com>
2015-03-03 15:17:30 -0800

    completed links, layout, and second transparent sections

--- public/javascripts/postcaptain.js
+++ public/javascripts/postcaptain.js
@@ -6,12 +6,25 @@ $( document ).ready(function() {
     
     // console.log($(this).scrollHeight())
     // var currentHeight = this.scrollHeight - this.scrollTop
-    if ($(this).scrollTop() > window.innerHeight/2) {
+    if ($(this).scrollTop() > window.innerHeight/1.5) {
       $('#slider').slideDown();
+      // $('#project-row').removeAttr('height');
     }
-    if ($(this).scrollTop() < window.innerHeight/2) {
+    if ($(this).scrollTop() < window.innerHeight/1.5) {
       $('#slider').slideUp();
+      // $('#project-row').css('height: 40vh;')
     }
+
+    if ($(this).scrollTop() > window.innerHeight) {
+      $('.header h1').hide();
+    }
+
+    if ($(this).scrollTop() < window.innerHeight) {
+      $('.header h1').show();
+    }
+
+
+
   }); 
  $('.thumbnail').hover(
         function(){
